f6a03448490ed4d703e757061f940a4968597e05
Allan Monteiro <allan_mlpe@hotmail.com>
2017-01-29 15:10:08 -0300

    Correção de código de operadora

--- aula7/database.js
+++ aula7/database.js
@@ -9,7 +9,7 @@
 */
 var operadoras = [
 	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
-	{nome: "Vivo", codigo: 14, categoria: "Celular", preco: 1},
+	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
 	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3}
 ];
 
